528f91ff4b4dbe067360800eb61b3fae23edeba3
aavantika <38779309+aavantika@users.noreply.github.com>
2020-10-09 15:53:44 +0530

    Update skeleton.py

--- skeleton.py
+++ skeleton.py
@@ -1,24 +1,35 @@
 # this is the skeleton python code.
 
-
 # Soma
-#def getFirstWord():
+def getFirstWord(string):
 	# Write the code here
-
+   word_list = (string.split())
+   return word_list[0]	
 
 
 # Avantika
-#def getLastWord(string):
+def getLastWord(string):
    word_list = str.split()
    return word_list[-1]
 	
 
 
 # Paddy
-#def getNumWords():
+def getNumWords(string):
 	# Write code here
-	#print("Hello!")
-	return "hello!"
+	return(len(string.split()))
+
+
+def swapCase(string):
+    swapcase= str[-1:] + str[1:-1] + str[:1]
+    return(swapcase)
+
+def exptWork(string):
+    return('This is expt ' + string)
+
+
+def exptWork2(string):
+    return('This is expt 2 ' + string)
 
 
 str = 'The Good The Bad and The Ugly'
@@ -26,6 +37,18 @@ print(str)
 print(getFirstWord(str))
 print(getLastWord(str))
 print(getNumWords(str))
+print(swapCase(str))
+print(exptWork(str))
+print(exptWork2(str))
+
+str = 'The Shawshank Redumption'
+print(str)
+print(getFirstWord(str))
+print(getLastWord(str))
+print(getNumWords(str))
+print(swapCase(str))
+print(exptWork(str))
+print(exptWork2(str))
 
 
 
